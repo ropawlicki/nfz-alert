a361181e437df1ba82291f65873b379e18f0bafb
Robert <r-pawlicki@wp.pl>
2020-12-07 11:24:51 +0100

    fix indentation in create_query.rb

--- app/services/create_query.rb
+++ app/services/create_query.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class CreateQuery
-    def self.call(params, user_id)
-      params.each_value(&:capitalize!)
-      query = Query.find_by(params)
-      if query.nil?
-        query = Query.create(params)
-        InitialSearch.call(query.id)
-      end
-      query.user_queries.create(user_id: user_id)
-      query
+  def self.call(params, user_id)
+    params.each_value(&:capitalize!)
+    query = Query.find_by(params)
+    if query.nil?
+      query = Query.create(params)
+      InitialSearch.call(query.id)
     end
+    query.user_queries.create(user_id: user_id)
+    query
   end
+end
